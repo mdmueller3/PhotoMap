f13aec9a78134e45008025130ba3be3b87d7e4e3
Michael Mueller <michaeldmueller3@gmail.com>
2015-12-07 17:59:20 -0600

    Fixed bugs, added comments, added more methods

--- photoMap.js
+++ photoMap.js
@@ -1,7 +1,11 @@
 var listOfStates = [];
-var images = [];
+var statesWithImages = [];
 //eventually combine these
+
 var hoverEffect = false;
+var hoverColor;
+var exitColor;
+var transition;
 
 function photomap(posX, posY, width, height, scale){
 	var paper = Raphael(posX,posY,width,height);
@@ -22,33 +26,39 @@ function photomap(posX, posY, width, height, scale){
 
 		pullImages(state, listOfStates[state.id]);
 
-
 	};
 	var elementSet = paper.setFinish();
 
+	this.setHoverEffect = function(hover, exit, transition){
+		hoverEffect = true;
+		hoverColor = hover;
+		exitColor = exit;
+		transitionTime = transition;
+	};
 
+	this.setSize = function(width, height){
+		paper.setSize(width, height);
+	};
 
-	this.setHoverEffect = function(hoverColor, exitColor, transitionTime){
+	this.setWidth = function(width){
+		paper.setSize(width, paper.height);
+	};
 
-		var over = function(){
-			this.stop().animate({fill: hoverColor.toString()}, transitionTime);
-		};
-
-		var out = function(){
-			this.stop().animate({fill: exitColor.toString()}, transitionTime);
-		};
-		elementSet.hover(over, out);
-	}
+	this.setHeight = function(height){
+		paper.setSize(paper.width, height);
+	};
 
 	return this;
-
 };
 
 function pathExists(path, callback){
 	var img = new Image();
 	// img.onerror = function(){alert("error"); return false;};
 
+	// What occurs on load
 	img.onload = function(){callback(true);};
+
+	// What occurs on error
 	img.onerror = function(){callback(false);};
 
 	img.src = path;
@@ -59,21 +69,39 @@ function pullImages(state, id){
 	// alert(id); // returns the initials of the state (the path)
 	var path = 'images/'.concat(id).concat('/0.jpg');
 
+	// Path exists function - checks whether the path exists
 	pathExists(path, function(exists){
+
+		// Checks if path exists
 		if(exists){
+			// if path exists, use url to fill state
 			var url = 'url('.concat(path).concat(')');
 			state.attr('fillfit',url);
 		}
+		// If path doesn't exist, but hoverEffect is true, add hover effect
+		else if(hoverEffect == true){
+			// Hover effect
+			var over = function(){
+				state.stop().animate({fill: hoverColor}, transitionTime);
+			}
+			// Exit effect
+			var out = function(){
+				state.stop().animate({fill: exitColor}, transitionTime);
+			};
+
+			// Add the hover
+			state.hover(over, out);
+		};
 	});
 
 }
 
 
 function hideImages(state, id){
-	for(var i = 0; i < images.length; i++){
-		if(images[i].id == id){
-			var image = images[i];
-			images.splice(i, 1);
+	for(var i = 0; i < statesWithImages.length; i++){
+		if(statesWithImages[i].id == id){
+			var image = statesWithImages[i];
+			statesWithImages.splice(i, 1);
 
 			var div = document.getElementById('holder');
 			div.removeChild(image);
--- photoMapTesting.js
+++ photoMapTesting.js
@@ -1,2 +1,3 @@
 var map = photomap(100,100,1000,1000,1);
 map.setHoverEffect("#eeffff", "white", 50);
+// map.setHeight(100);
